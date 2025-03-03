16337ba9bd5ec2b38fafd9b9b27d7c1843fffb27
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-03-03 14:35:51 -0300

    logica do item.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import createitem from "./services/item.js";
+
 const cart = [];
 
-
-
 console.log("Hello Shopee");
+
+const item1 = await createitem("hotwheels ferrari", 20.99, 1);
+const item2 = await createitem("hotwheels lamborghini", 39.99, 3);
+
+console.log(item2.subtotal());
--- src/services/item.js
+++ src/services/item.js
@@ -2,5 +2,12 @@
 
 //->criar item com uso subtotal certo
 async function createitem(name, price, quantity) {
-    
+    return{
+        name,
+        price,
+        quantity,
+        subtotal: ()=> price * quantity,
+    };
 }
+
+export default createitem;
