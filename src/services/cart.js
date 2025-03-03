d587568c786d2e629b806407453ef08524a24e5e
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-03-03 14:56:28 -0300

    card add e card subtotal

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,19 +1,27 @@
 //quais açoes meu carrinho pode fazer?
 
 //casos de uso
-//->adicionar item no carrinho
-async function addItem(userCart){
-
+//✅->adicionar item no carrinho
+async function addItem(userCart, item){
+    userCart.push(item);
 }
 //->deletar item do carrinho
 async function deletItem(userCart, name) {
     
 }
 //->remover um item - diminui um item
-async function removeIten(userCart, index) {
+async function removeItem(userCart, index) {
     
 }
 //->calcular o total do carrinho
 async function calculateTotal(userCart) {
-    
+    return userCart.reduce((total, item)=> total + item.subtotal(), 0);
 }
+
+
+export {
+    addItem,
+    calculateTotal,
+    deletItem,
+    removeItem
+};
