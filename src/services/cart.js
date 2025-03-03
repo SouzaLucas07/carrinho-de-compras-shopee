6b43da7978c4c1f62373d57dfcb8313f94e31e99
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-03-03 14:25:33 -0300

    assinaturas dos services.js

---
+++ src/services/cart.js
@@ -0,0 +1,19 @@
+//quais açoes meu carrinho pode fazer?
+
+//casos de uso
+//->adicionar item no carrinho
+async function addItem(userCart){
+
+}
+//->deletar item do carrinho
+async function deletItem(userCart, name) {
+    
+}
+//->remover um item - diminui um item
+async function removeIten(userCart, index) {
+    
+}
+//->calcular o total do carrinho
+async function calculateTotal(userCart) {
+    
+}
---
+++ src/services/item.js
@@ -0,0 +1,6 @@
+//casos de uso dos itens
+
+//->criar item com uso subtotal certo
+async function createitem(name, price, quantity) {
+    
+}
